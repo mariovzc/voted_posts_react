316f2ead2de25500e02f38989a55220ef4101ca1
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-02-27 18:51:33 -0500

    display multiples posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-
+import posts from './posts'
 class App extends Component {
+  constructor () {
+    super()
+    this.state= {
+      posts: [],
+      order: 'ASC'
+    }
+  }
   render() {
     return (
       <div className="App">  
-        <h1>App</h1>    
+        <h1>Blog posts populares</h1>
+        {this.state.posts.map(post => {
+          return post.id
+        })}
+        <hr/>
       </div>
     );
+  }
+  componentWillMount() {
+    this.setState({
+      posts: posts
+    })
   }
 }
 
