7ce2048dff3a2546ae8a3527ffe2768c2d68fb63
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-02-27 19:11:25 -0500

    post item component created

---
+++ src/components/PostItem.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+/**
+ * POST ITEM PROPS
+  id,
+  title,
+  description,
+  url,
+  votes,
+  writer_avatar_url,
+  post_image_url
+ */
+class PostItem extends Component {
+  render () {
+    return (
+      <div>
+        <div>
+          <img 
+            src={this.props.post_image_url}
+            alt={this.props.title}
+          />
+        </div>
+        <div>
+          <button>UpVote</button>
+          <span>{this.props.votes}</span>
+          <button>DownVote</button>
+        </div>
+        <div>
+          <h3><a href={this.url} target='_blank'>{this.props.title}</a></h3>
+          <p>{this.props.description}</p>
+          <small>
+            Escrito por: 
+            <img 
+              src={this.props.writer_avatar_url}
+              alt='AVATAR'
+             />
+          </small>
+        </div>
+      </div>
+    )
+  }
+}
+export default PostItem
